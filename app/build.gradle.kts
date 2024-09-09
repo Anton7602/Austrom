b6c4a5e13fc085b862aa283df404609551ad3e0b
Anton7602 <Rob.kov96@gmail.com>
2024-09-09 17:54:32 +0300

    Implemented Assets addition functionality, Added main Navigation Drawer, Improved Bottom Navigation Bar behaviour, Switched target API from 34 to 35 (useless so far, would be ok to go back)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.colleagues.austrom"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.colleagues.austrom"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
