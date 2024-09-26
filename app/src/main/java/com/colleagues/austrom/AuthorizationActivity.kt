8198febb0d8451e2ebbc67ac64c0dcaf1148a32b
Anton7602 <Rob.kov96@gmail.com>
2024-09-26 16:12:22 +0300

    Added quick auth with pin

--- app/src/main/java/com/colleagues/austrom/AuthorizationActivity.kt
+++ app/src/main/java/com/colleagues/austrom/AuthorizationActivity.kt
@@ -3,26 +3,17 @@ package com.colleagues.austrom
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
-import android.os.Build
 import android.os.Bundle
-import android.provider.Settings
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.biometric.BiometricManager.Authenticators.BIOMETRIC_STRONG
-import androidx.biometric.BiometricManager.Authenticators.DEVICE_CREDENTIAL
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.lifecycle.lifecycleScope
 import com.colleagues.austrom.database.FirebaseDatabaseProvider
 import com.colleagues.austrom.database.IDatabaseProvider
-import com.colleagues.austrom.managers.BiometricPromptManager
-import com.colleagues.austrom.models.User
 import com.google.android.material.textfield.TextInputEditText
-import kotlinx.coroutines.launch
 
 class AuthorizationActivity : AppCompatActivity() {
     private lateinit var logInButton: Button
@@ -31,10 +22,7 @@ class AuthorizationActivity : AppCompatActivity() {
     private lateinit var loginTextBox : TextInputEditText
     private lateinit var passwordTextBox : TextInputEditText
     private lateinit var sharedPreferences : SharedPreferences
-    private var promptResults: BiometricPromptManager.BiometricResult? = null
-    private val promptManager by lazy{
-        BiometricPromptManager(this)
-    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -46,7 +34,10 @@ class AuthorizationActivity : AppCompatActivity() {
             insets
         }
         bindViews()
-        tryAuthorizeExistingUserOnDevice()
+        val editor = sharedPreferences.edit()
+        editor.putString("appQuickPin","7602")
+        editor.apply()
+        runQuickAuthorization()
 
         logInButton.setOnClickListener{
             val dbProvider: IDatabaseProvider = FirebaseDatabaseProvider(this)
@@ -81,47 +72,16 @@ class AuthorizationActivity : AppCompatActivity() {
         }
     }
 
-    private fun tryAuthorizeExistingUserOnDevice() {
+    private fun runQuickAuthorization() {
         val storedUserID = sharedPreferences.getString("appUserId", "")
-        if (!storedUserID.isNullOrEmpty()) {
+        val storedPin = sharedPreferences.getString("appQuickPin", "")
+        if (!storedUserID.isNullOrEmpty() && !storedPin.isNullOrEmpty()) {
             val dbProvider: IDatabaseProvider = FirebaseDatabaseProvider(this)
             val existingUser = dbProvider.getUserByUserId(storedUserID)
             if (existingUser!= null) {
-                initializeBiometricAuthentication(existingUser)
+                startActivity(Intent(applicationContext, AuthorizationQuickActivity::class.java))
             }
         }
-    }
-
-    private fun initializeBiometricAuthentication(user: User) {
-        lifecycleScope.launch {
-            promptManager.promptResults.collect { results ->
-                promptResults = results
-                when(promptResults) {
-                    BiometricPromptManager.BiometricResult.AuthenticationError -> Toast.makeText(applicationContext, "Authentication Error", Toast.LENGTH_LONG).show()
-                    BiometricPromptManager.BiometricResult.AuthenticationFailed -> Toast.makeText(applicationContext, "Authentication Failed", Toast.LENGTH_LONG).show()
-                    BiometricPromptManager.BiometricResult.AuthenticationNotSet -> Toast.makeText(applicationContext, "Authentication Not Set", Toast.LENGTH_LONG).show()
-                    BiometricPromptManager.BiometricResult.AuthenticationSuccess -> {
-                        AustromApplication.appUser = user
-                        startActivity(Intent(applicationContext, MainActivity::class.java))
-                    }
-                    BiometricPromptManager.BiometricResult.FeatureUnavailable -> Toast.makeText(applicationContext, "Authentication Feature Unavailable", Toast.LENGTH_LONG).show()
-                    BiometricPromptManager.BiometricResult.HardwareUnavailable -> Toast.makeText(applicationContext, "Authentication Hardware Unavailable", Toast.LENGTH_LONG).show()
-                    else ->  Toast.makeText(applicationContext,  "??", Toast.LENGTH_LONG).show()
-                    }
-                if (promptResults is BiometricPromptManager.BiometricResult.AuthenticationNotSet && Build.VERSION.SDK_INT>=30) {
-                    val activityLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {result ->
-                        if (result.resultCode == RESULT_OK) {
-                            Toast.makeText(applicationContext,  result.data.toString(), Toast.LENGTH_LONG).show()
-                        }
-                    }
-                    val enrollIntent = Intent(Settings.ACTION_BIOMETRIC_ENROLL).apply {
-                        putExtra(Settings.EXTRA_BIOMETRIC_AUTHENTICATORS_ALLOWED, BIOMETRIC_STRONG or DEVICE_CREDENTIAL)
-                    }
-                    activityLauncher.launch(enrollIntent)
-                }
-            }
-        }
-        promptManager.showBiometricPrompt(this.getString(R.string.biometric_title), this.getString(R.string.biometric_prompt))
     }
 
     private fun bindViews() {
