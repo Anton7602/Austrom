258f3821235329576fcbb76fc6d4b30130d63ce0
Anton7602 <Strateg7602@gmail.com>
2024-10-03 09:37:16 +0300

    And another minor hotfix

--- app/src/main/java/com/colleagues/austrom/AssetPropertiesActivity.kt
+++ app/src/main/java/com/colleagues/austrom/AssetPropertiesActivity.kt
@@ -37,11 +37,7 @@ class AssetPropertiesActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_asset_properties)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        adjustInsets()
         bindViews()
         retrieveAssetFromIntent()
         setUpAssetProperties()
@@ -98,6 +94,14 @@ class AssetPropertiesActivity : AppCompatActivity() {
         transactionHolder.layoutManager = LinearLayoutManager(this)
         val groupedTransactions = Transaction.groupTransactionsByDate(transactionsOfAsset)
         transactionHolder.adapter = TransactionGroupRecyclerAdapter(groupedTransactions, this)
+    }
+
+    private fun adjustInsets() {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
     }
 
     private fun bindViews() {
